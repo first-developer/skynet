5bd183183aa663da5a34bcd508aac3062fc36351
first-developer <lioninho11@hotmail.fr>
2016-02-28 18:55:32 +0100

    Add Game class

---
+++ game.js
@@ -0,0 +1,23 @@
+/**
+ * Constants
+ */
+let SPACE_CHARACTER = " " 
+
+/**
+ * Game.JS
+ */
+var Game = {
+	loopCount: null,	// the number of game loop 
+	N: null,			// the total number of nodes in the level, including the gateways
+	L: null,			// the number of links
+	E: null,			// the number of exit
+	nodes: null,
+	links: null,
+	agentPositions: [] // The list of different positions taken by the Skynet agent.
+}
+
+
+
+
+
+
