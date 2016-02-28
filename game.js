f3c5db70a29a4b69d904f1d6bcde7db911084d2f
first-developer <lioninho11@hotmail.fr>
2016-02-28 19:13:16 +0100

    Change the Game class definition style

--- game.js
+++ game.js
@@ -6,16 +6,40 @@ let SPACE_CHARACTER = " "
 /**
  * Game.JS
  */
-var Game = {
-	loopCount: null,	// the number of game loop 
-	N: null,			// the total number of nodes in the level, including the gateways
-	L: null,			// the number of links
-	E: null,			// the number of exit
-	nodes: null,
-	links: null,
-	agentPositions: [] // The list of different positions taken by the Skynet agent.
-}
+var Game = function(){
+	// Attributes 
+	this.loopCount = null,	// the number of game loop 
+	this.N = null,			// the total number of nodes in the level, including the gateways
+	this.L = null,			// the number of links
+	this.E = null,			// the number of exit
+	this.nodes = null, 		// the all nodes present in the game.
+	this.links = null,		// the links present in the game. 
+	this.agentPositions = [] // The list of different positions taken by the Skynet agent.
 
+
+
+	// Utils functions 
+	// ----------------
+
+	/**
+	 * Return the user intput as a single value
+	 * @return {Integer} 
+	 */
+	Game.prototype.input = function(){
+		return parseInt(readline());	
+	}
+
+	/**
+	 * Return the user intput as a list of values
+	 * @return {Array[Integer]} 
+	 */
+	Game.prototype.inputs = function(){
+		return readline().split(SPACE_CHARACTER)
+			.map((x) => { parseInt(x); });	;
+	}
+
+}
+	
 
 
 
