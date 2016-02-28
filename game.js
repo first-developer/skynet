aa4e15051b207d7e774d66fe8d4b5eec80a3f371
first-developer <lioninho11@hotmail.fr>
2016-02-29 00:41:51 +0100

    Add 'Node' and 'Link' class

--- game.js
+++ game.js
@@ -7,15 +7,39 @@ const SPACE_CHARACTER = " ";
  * Deug function
  * @param  {...Integer} items 
  */
-var d = function(...items){
+var d = function(...items) {
     let logText = items.map((x) => JSON.stringify(x)).join(SPACE_CHARACTER);    
     printErr(logText);
 }
 
+
+/**
+ * Node object
+ */
+var Node = function(id, reachable) { 
+    this.id         = id || null;
+    this.reachable  = reachable || false 
+}
+Node.prototype.isReachable = function() {
+    return this.reachable;
+}
+
+
+
+/**
+ * Link object
+ */
+var Link = function(N1, N2) {
+    this.N1 = N1;
+    this.N2 = N2;
+}
+
+
+
 /**
  * Game.JS
  */
-var Game = function(context, options){
+var Game = function(context, options) {
     let opts = options === undefined ? {} : options;
     
     // Attributes 
@@ -28,55 +52,63 @@ var Game = function(context, options){
     
     this.loopCount      = null;             // the number of game loop 
     this.agentPositions = [];               // The list of different positions taken by the Skynet agent.
-
     
+    let defaultInputReader = function(context) {
+        let ctx = context || this;
+    
+        return {
+            /**
+             * Return the user intput as a single value
+             * @return {Integer} 
+             */
+            input:  function() {
+                return parseInt(ctx.readline());    
+            },
+
+            /**
+             * Return the user intput as a list of values
+             * @return {Array[Integer]} 
+             */
+            inputs: function() {
+                return ctx.readline().split(SPACE_CHARACTER).map((x) => { return parseInt(x); });
+            },
+        
+        }
+    }
     this.console = opts.console || this.defaultInputReader(context);
+
+    // Initializer 
+    let initialize = function() {
+        if ( !(this.N && this.L && this.E) ) {
+            let inputs = this.console.inputs();
+            this.N = inputs[0]; // the total number of nodes in the level, including the gateways
+            this.L = inputs[1]; // the number of links
+            this.E = inputs[2]; // the number of exit gateways    
+        }
+        
+        if ( !(this.links && this.nodes) ) {
+            for (var i = 0; i < this.L; i++) {
+                var inputs = g.console.inputs();
+                var N1 = inputs[0]; // N1 and N2 defines a link between these nodes
+                var N2 = inputs[1];
+                
+            }
+        }
+        
+        for (var i = 0; i < g.E; i++) {
+            var EI = g.console.input(); // the index of a gateway node
+            d("EI", EI);
+        }
+    }
     
 };
 
-Game.prototype.defaultInputReader = function(context){
-    let ctx = context || this;
-    
-    return {
-        /**
-         * Return the user intput as a single value
-         * @return {Integer} 
-         */
-        input:  function(){
-            return parseInt(ctx.readline());    
-        },
-
-        /**
-         * Return the user intput as a list of values
-         * @return {Array[Integer]} 
-         */
-        inputs: function(){
-            return ctx.readline().split(SPACE_CHARACTER).map((x) => { return parseInt(x); });
-        },
-    
-    }
 };
 
 
 var g = new Game(this);
 
-var inputs = g.console.inputs();
-g.N = inputs[0]; // the total number of nodes in the level, including the gateways
-g.L = inputs[1]; // the number of links
-g.E = inputs[2]; // the number of exit gateways
 
-
-for (var i = 0; i < g.L; i++) {
-    var inputs = g.console.inputs();
-    var N1 = inputs[0]; // N1 and N2 defines a link between these nodes
-    var N2 = inputs[1];
-    d("(N1,N2)",N1,N2);    
-    
-}
-for (var i = 0; i < g.E; i++) {
-    var EI = g.console.input(); // the index of a gateway node
-    d("EI", EI);
-}
 
 // game loop
 while (true) {
